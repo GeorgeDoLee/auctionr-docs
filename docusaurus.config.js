ffcb4c6080472565b17d55b96e113836ce6eb2a3
georgedolee <58554331+georgedolee@users.noreply.github.com>
2025-07-03 12:30:36 +0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,14 +20,14 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://GeorgeDoLee.github.io',
+  url: 'https://georgedolee.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/auctionr-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'GeorgeDoLee', // Usually your GitHub org/user name.
+  organizationName: 'georgedolee', // Usually your GitHub org/user name.
   projectName: 'auctionr-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -106,7 +106,7 @@ const config = {
             position: 'left',
           },
           {
-            href: 'https://github.com/GeorgeDoLee/auctionr/',
+            href: 'https://github.com/georgedolee/auctionr/',
             label: 'GitHub',
             position: 'right',
           },
@@ -133,11 +133,11 @@ const config = {
               },
               {
                 label: 'Issues',
-                href: 'https://github.com/GeorgeDoLee/auctionr/issues/',
+                href: 'https://github.com/georgedolee/auctionr/issues/',
               },
               {
                 label: 'Discussions',
-                href: 'https://github.com/GeorgeDoLee/auctionr/discussions/',
+                href: 'https://github.com/georgedolee/auctionr/discussions/',
               },
             ],
           },
@@ -155,7 +155,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/GeorgeDoLee/auctionr/',
+                href: 'https://github.com/georgedolee/auctionr/',
               },
               {
                 label: 'LinkedIn',
